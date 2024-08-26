07cbcea5e6c5c47bdfe1eaaa7882a845a0a5000b
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-08-26 21:42:06 +0530

    Update CreateUser.jsx

--- CRUD/client/src/CreateUser.jsx
+++ CRUD/client/src/CreateUser.jsx
@@ -10,7 +10,7 @@ const CreateUser = () => {
 
     const Submit=(e)=>{
         e.preventDefault();
-        axios.post("http://localhost:3001/createUser",{name,email,age})
+        axios.post("https://mern-notes-nojq.onrender.com/createUser",{name,email,age})
         .then(result=>{
             console.log(result),
             navigate('/')
