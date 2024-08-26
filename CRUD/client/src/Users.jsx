e4338776ffc17ee5f22f2e13042475c1c0e92156
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-08-26 21:43:04 +0530

    Update Users.jsx

--- CRUD/client/src/Users.jsx
+++ CRUD/client/src/Users.jsx
@@ -6,13 +6,13 @@ function Users() {
   const [users, setUsers] = useState([])
 
   useEffect(()=>{
-    axios.get("http://localhost:3001/")
+    axios.get("https://mern-notes-nojq.onrender.com/")
     .then(result=>setUsers(result.data))
     .catch(err=>console.log(err))
   },[])
 
 const handleDelete=(id)=>{
-    axios.delete('http://localhost:3001/deleteUser/'+id)
+    axios.delete('https://mern-notes-nojq.onrender.com/deleteUser/'+id)
     .then(res=>{console.log(res)
         window.location.reload()
     })
